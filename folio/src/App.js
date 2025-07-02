6b7271d135245929db3fae11ae8616b6a0a166a4
FranciscoCamach0 <129994954+FranciscoCamach0@users.noreply.github.com>
2025-07-01 19:14:45 -0700

    Update App.js

--- folio/src/App.js
+++ folio/src/App.js
@@ -1,13 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/HomePage';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-       <HomePage />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
+  </Router>
   );
 }
 
