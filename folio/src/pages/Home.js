10c5bc427c6a4b870062c8af3a3512baaffe91fc
FranciscoCamach0 <129994954+FranciscoCamach0@users.noreply.github.com>
2025-07-01 18:32:05 -0700

    test

---
+++ folio/src/pages/Home.js
@@ -0,0 +1,26 @@
+// Home.js
+
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <h1>Welcome to the Home Page</h1>
+        <p>
+          This is your new Home component. Edit <code>src/Home.js</code> and save to see changes.
+        </p>
+        <a
+          className="Home-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
