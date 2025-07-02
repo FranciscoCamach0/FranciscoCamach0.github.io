1d31ef8b4ec920e1ead52953901e914ba5a36568
FranciscoCamach0 <129994954+FranciscoCamach0@users.noreply.github.com>
2025-07-01 19:06:28 -0700

    Update HomePage.js

---
+++ folio/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+// src/pages/HomePage.js
+
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is the homepage of my React site deployed with GitHub Pages.</p>
+    </div>
+  );
+}
+
+export default HomePage;
