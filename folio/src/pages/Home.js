3b637f0d5cccd81f42164e26c91b1115a1f0501f
FranciscoCamach0 <129994954+FranciscoCamach0@users.noreply.github.com>
2025-07-01 18:40:59 -0700

    Update Home.js

--- folio/src/pages/Home.js
+++ folio/src/pages/Home.js
@@ -1,16 +1,12 @@
-// home.js
-
-import React from 'react';
+// Home.js
 
 function Home() {
-  return (
-    <div className="Home">
-      <header className="Home-header">
-        <h1>Welcome to the Home Page</h1>
-        <p>Edit <code>src/home.js</code> and save to see changes.</p>
-      </header>
-    </div>
-  );
-}
-
-export default Home;
+    return (
+      <div>
+        <h1>Home Page</h1>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
