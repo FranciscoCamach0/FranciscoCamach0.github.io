f8430b785afafcd820b1dbf00b2b435f846cbdff
FranciscoCamach0 <129994954+FranciscoCamach0@users.noreply.github.com>
2025-07-01 18:34:46 -0700

    Update Home.js

--- folio/src/pages/Home.js
+++ folio/src/pages/Home.js
@@ -1,4 +1,4 @@
-// Home.js
+// home.js
 
 import React from 'react';
 
@@ -7,17 +7,7 @@ function Home() {
     <div className="Home">
       <header className="Home-header">
         <h1>Welcome to the Home Page</h1>
-        <p>
-          This is your new Home component. Edit <code>src/Home.js</code> and save to see changes.
-        </p>
-        <a
-          className="Home-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Edit <code>src/home.js</code> and save to see changes.</p>
       </header>
     </div>
   );
