635debfcfbb58d34fa18ad588c24a17bbd2e9fc5
FranciscoCamach0 <129994954+FranciscoCamach0@users.noreply.github.com>
2025-07-02 18:21:06 -0700

    Update Navbar.js

--- folio/src/components/Navbar.js
+++ folio/src/components/Navbar.js
@@ -7,6 +7,8 @@ function Navbar() {
   return (
     <nav className="navbar">
       <Link to="/">Home</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/about">About</Link>
     </nav>
   );
 }
