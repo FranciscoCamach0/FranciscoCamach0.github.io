ef7e7bbcdcdd27fb9754f8c9e30d11862930a529
FranciscoCamach0 <129994954+FranciscoCamach0@users.noreply.github.com>
2025-07-02 18:56:40 -0700

    Update About.js

--- folio/src/pages/About.js
+++ folio/src/pages/About.js
@@ -3,7 +3,11 @@ import React from 'react';
 function About() {
     return (
         <div>
-            
+            <h1>About Me</h1>
+            <p>
+            I’m a recent Computer Science graduate from California State University, Monterey Bay, with a concentration in Software Engineering. I first discovered my passion for coding in high school and have been hooked ever since. I went on to complete the CSin3 program, an accelerated pathway that allowed me to earn my Bachelor’s degree in just three years while developing strong skills in software design, development, and teamwork.
+
+            </p>
         </div>
     );
 }
