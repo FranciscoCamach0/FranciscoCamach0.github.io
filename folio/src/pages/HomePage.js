ac1206b5a5b99af66d2a26002ae556437e52d199
FranciscoCamach0 <129994954+FranciscoCamach0@users.noreply.github.com>
2025-07-04 18:32:41 -0700

    update

--- folio/src/pages/HomePage.js
+++ folio/src/pages/HomePage.js
@@ -19,7 +19,7 @@ function HomePage() {
 
         <div className = "updown">
 
-          <p> I’m a recent Computer Science graduate from California State University, Monterey Bay, with a concentration in Software Engineering. I first discovered my passion for coding in high school and have been hooked ever since. I went on to complete the CSin3 program, an accelerated pathway that allowed me to earn my Bachelor’s degree in just three years while developing strong skills in software design, development, and teamwork.</p>
+          <p className = "homepageText"> I’m a recent Computer Science graduate from California State University, Monterey Bay, with a concentration in Software Engineering. I first discovered my passion for coding in high school and have been hooked ever since. I went on to complete the CSin3 program, an accelerated pathway that allowed me to earn my Bachelor’s degree in just three years while developing strong skills in software design, development, and teamwork.</p>
           <span></span>
 
           <button className = "contactButton"> Contact </button>
@@ -43,6 +43,7 @@ function HomePage() {
           </div>
       
         </div>
+        <img className = "selfie" src={selfie} alt="picture of francisco" />
 
 
       </div>
