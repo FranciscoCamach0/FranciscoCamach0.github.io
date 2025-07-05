5ec40732a5d28621b2a3adddabe4387207acd802
FranciscoCamach0 <129994954+FranciscoCamach0@users.noreply.github.com>
2025-07-04 18:24:46 -0700

    test

--- folio/src/pages/HomePage.js
+++ folio/src/pages/HomePage.js
@@ -3,14 +3,62 @@
 import React from 'react';
 import '../css/background.css';
 import selfie from '../images/selfie.jpg';
+import linkedIn from '../images/linkedIn.jpg'
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div>
       <h1>Welcome to My Portfolio</h1>
-      <p>This is the homepage of my React site deployed with GitHub Pages.</p>
+      <p> Hellp I'm </p>
+      <h2> Francisco Camacho </h2>
+      <h3> Software Engineer </h3>
 
+      <span> </span>
       <div className = "homePageDiv">
+
+        <div className = "updown">
+
+          <p> I’m a recent Computer Science graduate from California State University, Monterey Bay, with a concentration in Software Engineering. I first discovered my passion for coding in high school and have been hooked ever since. I went on to complete the CSin3 program, an accelerated pathway that allowed me to earn my Bachelor’s degree in just three years while developing strong skills in software design, development, and teamwork.</p>
+          <span></span>
+
+          <button className = "contactButton"> Contact </button>
+          <div className = "links"> 
+            <a
+                className="linkedinButton"
+                href="https://www.linkedin.com/in/francisco-solis-a51a962b1/"
+                target="_blank"
+              >
+                <img className="linkedInlogo" src={linkedIn} alt="LinkedIn logo" />
+              </a>`
+
+            <a
+              className="githubButton"
+              href="https://github.com/FranciscoCamach0"
+              target="_blank"
+            >
+              <img className="linkedInlogo" src={linkedIn} alt="GitHub logo" />
+            </a>
+
+          </div>
+      
+        </div>
+
+
+      </div>
+      
+      <h1>About Me </h1>
+     
+
+    </div>
+  );
+}
+
+export default HomePage;
+
+
+
+{/* <div className = "homePageDiv">
         <div className = "updown">
             <h2>Software Delevoper</h2>
             <p>Back-End / Front-End </p>
@@ -24,11 +72,4 @@ function HomePage() {
         </div>
 
 
-        <p> </p>
-      </div>
-
-    </div>
-  );
-}
-
-export default HomePage;
+      </div> */}
